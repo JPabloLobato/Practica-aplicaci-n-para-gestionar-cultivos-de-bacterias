ece2a25e297f1c6e9cfb5878c3e7a05dd99562a2
JPabloLobato <jlobagon@myuax.com>
2024-05-04 23:28:28 +0200

    Commit GestorDatos

--- src/main/java/Navegador_Archivos/GestorDatos.java
+++ src/main/java/Navegador_Archivos/GestorDatos.java
@@ -1,41 +1,40 @@
-package Navegador_Datos;
+package Navegador_Archivos;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import Logica_de_Negocio.Experimento;
 import javax.swing.*;
 import java.io.*;
+import java.lang.reflect.Type;
 
 public class GestorDatos {
-    private static final String CARPETA_EXPERIMENTOS = "Experimentos/";
-    public static void guardarExperimento(String nombreArchivo, String contenido) {
-        // Crear la carpeta Experimentos si no existe
-        File carpetaExperimentos = new File(CARPETA_EXPERIMENTOS);
-        if (!carpetaExperimentos.exists()) {
-            carpetaExperimentos.mkdirs();
-        }
+    private static final String CARPETA_EXPERIMENTOS = "experimento/";
+    private static final Gson GSON = new Gson();
 
-        // Obtener la ruta del archivo donde se guardará el experimento
-        File archivo = new File(CARPETA_EXPERIMENTOS + nombreArchivo);
-        try (BufferedWriter writer = new BufferedWriter(new FileWriter(archivo))) {
-            writer.write(contenido);
-            JOptionPane.showMessageDialog(null, "Experimento guardado correctamente.");
-        } catch (IOException e) {
-            e.printStackTrace();
-            JOptionPane.showMessageDialog(null, "Error al guardar el experimento.");
+    public static void guardarExperimento(String nombreArchivo, Experimento experimento) {
+        try {
+            FileOutputStream fileOut = new FileOutputStream("Experimentos/" + nombreArchivo);
+            ObjectOutputStream out = new ObjectOutputStream(fileOut);
+            out.writeObject(experimento);
+            out.close();
+            fileOut.close();
+        } catch (IOException i) {
+            i.printStackTrace();
         }
     }
 
-    public static void guardarExperimentoComo(String contenido) {
-        // Crear la carpeta Experimentos si no existe
+    public static void guardarExperimentoComo(Experimento experimento) {
         File carpetaExperimentos = new File(CARPETA_EXPERIMENTOS);
         if (!carpetaExperimentos.exists()) {
             carpetaExperimentos.mkdirs();
         }
 
-        // Obtener la ruta y el nombre del archivo donde se guardará el experimento
         JFileChooser fileChooser = new JFileChooser(CARPETA_EXPERIMENTOS);
         int seleccion = fileChooser.showSaveDialog(null);
         if (seleccion == JFileChooser.APPROVE_OPTION) {
             File archivo = fileChooser.getSelectedFile();
             try (BufferedWriter writer = new BufferedWriter(new FileWriter(archivo))) {
+                String contenido = GSON.toJson(experimento);
                 writer.write(contenido);
                 JOptionPane.showMessageDialog(null, "Experimento guardado correctamente.");
             } catch (IOException e) {
@@ -45,25 +44,21 @@ public class GestorDatos {
         }
     }
 
-    public static String cargarExperimento() {
+    public static Experimento cargarExperimento() {
         JFileChooser fileChooser = new JFileChooser(CARPETA_EXPERIMENTOS);
         int seleccion = fileChooser.showOpenDialog(null);
         if (seleccion == JFileChooser.APPROVE_OPTION) {
             File archivo = fileChooser.getSelectedFile();
-            StringBuilder contenido = new StringBuilder();
             try (BufferedReader reader = new BufferedReader(new FileReader(archivo))) {
-                String linea;
-                while ((linea = reader.readLine()) != null) {
-                    contenido.append(linea).append("\n");
-                }
+                Type experimentoType = new TypeToken<Experimento>() {}.getType();
+                Experimento experimento = GSON.fromJson(reader, experimentoType);
                 JOptionPane.showMessageDialog(null, "Experimento cargado correctamente.");
+                return experimento;
             } catch (IOException e) {
                 e.printStackTrace();
                 JOptionPane.showMessageDialog(null, "Error al cargar el experimento.");
             }
-            return contenido.toString();
         }
         return null;
     }
-
 }
