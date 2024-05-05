89603b95b8b3caeeb99bc51f53e8c27523c757f0
JPabloLobato <jlobagon@myuax.com>
2024-05-05 18:07:08 +0200

    Commit GestorDatos

--- src/main/java/Navegador_Archivos/GestorDatos.java
+++ src/main/java/Navegador_Archivos/GestorDatos.java
@@ -1,11 +1,8 @@
 package Navegador_Archivos;
 
 import com.google.gson.Gson;
-import com.google.gson.reflect.TypeToken;
 import Logica_de_Negocio.Experimento;
-import javax.swing.*;
 import java.io.*;
-import java.lang.reflect.Type;
 
 public class GestorDatos {
     private static final String CARPETA_EXPERIMENTOS = "experimento/";
@@ -13,52 +10,28 @@ public class GestorDatos {
 
     public static void guardarExperimento(String nombreArchivo, Experimento experimento) {
         try {
-            FileOutputStream fileOut = new FileOutputStream("Experimentos/" + nombreArchivo);
-            ObjectOutputStream out = new ObjectOutputStream(fileOut);
-            out.writeObject(experimento);
-            out.close();
-            fileOut.close();
+            FileWriter fileWriter = new FileWriter("Experimentos/" + nombreArchivo);
+            GSON.toJson(experimento, fileWriter);
+            fileWriter.close();
         } catch (IOException i) {
             i.printStackTrace();
         }
     }
 
-    public static void guardarExperimentoComo(Experimento experimento) {
-        File carpetaExperimentos = new File(CARPETA_EXPERIMENTOS);
-        if (!carpetaExperimentos.exists()) {
-            carpetaExperimentos.mkdirs();
-        }
-
-        JFileChooser fileChooser = new JFileChooser(CARPETA_EXPERIMENTOS);
-        int seleccion = fileChooser.showSaveDialog(null);
-        if (seleccion == JFileChooser.APPROVE_OPTION) {
-            File archivo = fileChooser.getSelectedFile();
-            try (BufferedWriter writer = new BufferedWriter(new FileWriter(archivo))) {
-                String contenido = GSON.toJson(experimento);
-                writer.write(contenido);
-                JOptionPane.showMessageDialog(null, "Experimento guardado correctamente.");
-            } catch (IOException e) {
-                e.printStackTrace();
-                JOptionPane.showMessageDialog(null, "Error al guardar el experimento.");
-            }
+    public static Experimento cargarExperimento(String nombreArchivo) {
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader("Experimentos/" + nombreArchivo));
+            Experimento experimento = GSON.fromJson(reader, Experimento.class);
+            reader.close();
+            return experimento;
+        } catch (IOException i) {
+            i.printStackTrace();
+            return null;
         }
     }
 
-    public static Experimento cargarExperimento() {
-        JFileChooser fileChooser = new JFileChooser(CARPETA_EXPERIMENTOS);
-        int seleccion = fileChooser.showOpenDialog(null);
-        if (seleccion == JFileChooser.APPROVE_OPTION) {
-            File archivo = fileChooser.getSelectedFile();
-            try (BufferedReader reader = new BufferedReader(new FileReader(archivo))) {
-                Type experimentoType = new TypeToken<Experimento>() {}.getType();
-                Experimento experimento = GSON.fromJson(reader, experimentoType);
-                JOptionPane.showMessageDialog(null, "Experimento cargado correctamente.");
-                return experimento;
-            } catch (IOException e) {
-                e.printStackTrace();
-                JOptionPane.showMessageDialog(null, "Error al cargar el experimento.");
-            }
-        }
-        return null;
+    public static String[] obtenerNombresExperimentos() {
+        File folder = new File("Experimentos/");
+        return folder.list((dir, name) -> name.endsWith(".txt"));
     }
 }
