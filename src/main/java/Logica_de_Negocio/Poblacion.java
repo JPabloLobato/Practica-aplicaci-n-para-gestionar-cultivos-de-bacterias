400eb3f9947c8667308a4c4ac4e15ae9e0719477
JPabloLobato <jlobagon@myuax.com>
2024-05-06 21:53:24 +0200

    Commit Poblacion

--- src/main/java/Logica_de_Negocio/Poblacion.java
+++ src/main/java/Logica_de_Negocio/Poblacion.java
@@ -7,7 +7,10 @@ public class Poblacion {
     private int numBacteriasIniciales;
     private double temperatura;
     private String luminosidad;
-    private double dosisComida;
+    private int comidaInicial;
+    private int diaIncrementoComida;
+    private int comidaDiaIncremento;
+    private int comidaFinal;
 
     public String getNombre() {
         return nombre;
@@ -57,11 +60,49 @@ public class Poblacion {
         this.luminosidad = luminosidad;
     }
 
-    public double getDosisComida() {
-        return dosisComida;
+    public int getComidaInicial() {
+        return comidaInicial;
     }
 
-    public void setDosisComida(double dosisComida) {
-        this.dosisComida = dosisComida;
+    public void setComidaInicial(int comidaInicial) {
+        this.comidaInicial = comidaInicial;
+    }
+
+    public int getDiaIncrementoComida() {
+        return diaIncrementoComida;
+    }
+
+    public void setDiaIncrementoComida(int diaIncrementoComida) {
+        this.diaIncrementoComida = diaIncrementoComida;
+    }
+
+    public int getComidaDiaIncremento() {
+        return comidaDiaIncremento;
+    }
+
+    public void setComidaDiaIncremento(int comidaDiaIncremento) {
+        this.comidaDiaIncremento = comidaDiaIncremento;
+    }
+
+    public int getComidaFinal() {
+        return comidaFinal;
+    }
+
+    public void setComidaFinal(int comidaFinal) {
+        this.comidaFinal = comidaFinal;
+    }
+
+    @Override
+    public String toString() {
+        return "Nombre: " + nombre + "\n" +
+                "Fecha de inicio: " + fechaInicio + "\n" +
+                "Fecha de fin: " + fechaFin + "\n" +
+                "Número de bacterias iniciales: " + numBacteriasIniciales + "\n" +
+                "Temperatura: " + temperatura + "\n" +
+                "Luminosidad: " + luminosidad + "\n" +
+                "Comida inicial: " + comidaInicial + "\n" +
+                "Día de incremento de comida: " + diaIncrementoComida + "\n" +
+                "Comida del día de incremento: " + comidaDiaIncremento + "\n" +
+                "Comida final: " + comidaFinal + "\n";
     }
 }
---
+++ src/test/java/Logica_de_Negocio/PoblacionTest.java
@@ -0,0 +1,53 @@
+package Logica_de_Negocio;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PoblacionTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void getNombre() {
+    }
+
+    @Test
+    void getFechaInicio() {
+    }
+
+    @Test
+    void getFechaFin() {
+    }
+
+    @Test
+    void getNumBacteriasIniciales() {
+    }
+
+    @Test
+    void getTemperatura() {
+    }
+
+    @Test
+    void getLuminosidad() {
+    }
+
+    @Test
+    void getComidaInicial() {
+    }
+
+    @Test
+    void getDiaIncrementoComida() {
+    }
+
+    @Test
+    void getComidaDiaIncremento() {
+    }
+
+    @Test
+    void getComidaFinal() {
+    }
+}
